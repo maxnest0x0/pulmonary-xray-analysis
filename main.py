ac4f1e390bcf29e657095545d417f79373f3a51a
Max Nest <udomix.udomix@gmail.com>
2025-11-03 15:31:15 +0000

    Add dummy endpoint

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
 
 app = FastAPI()
+
+@app.post('/api/analyze')
+def analyze(image: UploadFile) -> None:
+    return None
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
